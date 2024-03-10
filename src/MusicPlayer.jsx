340593e47b584f9a8f12afdfe0391c584144db2d
Kushagra Gupta <kushagra357@gmail.com>
2024-03-10 18:06:16 +0530

    Works on Slider

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import "./MusicPlayer.css";
-import { Button, Slider } from "@mui/material";
-import { IconButton } from "@mui/material";
+import { Slider, IconButton } from "@mui/material";
 import { PlayArrow, Pause, SkipPrevious, SkipNext } from "@mui/icons-material";
 
 const MusicPlayer = ({ songs }) => {
@@ -10,6 +9,7 @@ const MusicPlayer = ({ songs }) => {
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [progress, setProgress] = useState(0);
+  const [duration, setDuration] = useState(0);
   const audioRef = useRef(null);
   const [imageStyle, setImageStyle] = useState({
     width: "26%", // Adjust image size for non-mobile devices
@@ -39,17 +39,6 @@ const MusicPlayer = ({ songs }) => {
     setIsPlaying(true); // Start playing previous song automatically
   };
 
-  const seekHandler = (event, newValue) => {
-    setProgress(newValue);
-  };
-
-  useEffect(() => {
-    const audioElement = audioRef.current;
-    if (audioElement && isFinite(audioElement.duration)) {
-      audioElement.currentTime = (progress * audioElement.duration) / 100;
-    }
-  }, [progress, songs[currentSongIndex].song]); // Add songs[currentSongIndex].song to dependencies
-
   useEffect(() => {
     const audioElement = audioRef.current;
     if (audioElement) {
@@ -61,6 +50,29 @@ const MusicPlayer = ({ songs }) => {
     }
   }, [isPlaying]);
 
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      if (audioRef.current) {
+        const currentTime = audioRef.current.currentTime;
+        setProgress(currentTime);
+      }
+    };
+
+    const audioElement = audioRef.current;
+    if (audioElement) {
+      audioElement.addEventListener("timeupdate", handleTimeUpdate);
+      return () => {
+        audioElement.removeEventListener("timeupdate", handleTimeUpdate);
+      };
+    }
+  }, [duration]);
+  const seekHandler = (event, newValue) => {
+    console.log("newValue", newValue);
+    setProgress(newValue);
+    if (audioRef.current) {
+      audioRef.current.currentTime = newValue;
+    }
+  };
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth <= 600) {
@@ -90,6 +102,30 @@ const MusicPlayer = ({ songs }) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (audioRef.current && isPlaying) {
+      const interval = setInterval(() => {
+        if (audioRef.current) {
+          const currentTime = audioRef.current.currentTime;
+          setProgress(currentTime);
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [isPlaying, duration]);
+
+  useEffect(() => {
+    // When a new song is selected, set the progress to 0
+    setProgress(0);
+  }, [currentSongIndex]);
+
+  useEffect(() => {
+    // When a new song is selected, update the duration of the song
+    setDuration(audioRef?.current?.duration);
+  }, [currentSongIndex]);
+  console.log("duraion", audioRef.current?.duration);
+  console.log("duraionState", duration);
+  console.log("currenttime", audioRef.current?.currentTime);
   return (
     <>
       <div
@@ -160,12 +196,12 @@ const MusicPlayer = ({ songs }) => {
           <div className="player" style={{ marginTop: "auto", zIndex: 15 }}>
             <div style={{ display: "flex", justifyContent: "center" }}>
               <Slider
-                sx={{ margin: "12px", width: "80%", color: "#fff" }} // Set color attribute to change slider color
+                sx={{ margin: "12px", width: "80%", color: "#fff" }}
                 value={progress}
                 onChange={seekHandler}
                 aria-labelledby="continuous-slider"
                 min={0}
-                max={100}
+                max={duration} // Set max value to duration
               />
             </div>
             <div>
@@ -174,6 +210,7 @@ const MusicPlayer = ({ songs }) => {
                 ref={audioRef}
                 src={songs[currentSongIndex].song}
                 autoPlay={isPlaying}
+                onLoadedMetadata={() => setDuration(audioRef.current.duration)}
               ></audio>
             </div>
             <div
